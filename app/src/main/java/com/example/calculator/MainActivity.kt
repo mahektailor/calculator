eb38ad4968a8a362a3de6406eaf101a416ce0a6a
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-07-07 21:32:27 -0400

    Linking multiple button to the function

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -29,5 +29,74 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inputEditText = findViewById(R.id.input_edit_text)
+        outputTextView = findViewById(R.id.output_text_view)
+        conversionSpinner = findViewById(R.id.conversion_spinner)
+        convertButton = findViewById(R.id.convert_button)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, conversionOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        conversionSpinner.adapter = adapter
+
+
+        conversionSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // Clear input and output fields when the conversion type is changed
+                inputEditText.text.clear()
+                outputTextView.text = ""
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do nothing
+            }
+        }
+
+        convertButton.setOnClickListener {
+            convert()
+        }
     }
+
+
+
+
+
+    private fun convert() {
+            val inputText = inputEditText.text.toString().trim()
+            if (inputText.isNotEmpty()) {
+                val inputValue = inputText.toDouble()
+
+                val conversionType = conversionSpinner.selectedItem.toString()
+                val result: Double
+
+                when (conversionType) {
+                    "cm to in" -> {
+                        result = inputValue / 2.54
+                        outputTextView.text = result.toString()
+                    }
+                    "in to cm" -> {
+                        result = inputValue * 2.54
+                        outputTextView.text = result.toString()
+                    }
+                    "km to m" -> {
+                        result = inputValue * 1000
+                        outputTextView.text = result.toString()
+                    }
+                    "m to km" -> {
+                        result = inputValue / 1000
+                        outputTextView.text = result.toString()
+                    }
+                    "ml to L" -> {
+                        result = inputValue / 1000
+                        outputTextView.text = result.toString()
+                    }
+                    "L to ml" -> {
+                        result = inputValue * 1000
+                        outputTextView.text = result.toString()
+                    }
+
+                }
+            }
+    }
+
 }
