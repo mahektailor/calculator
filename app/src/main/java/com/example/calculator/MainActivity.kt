a77cebedfcbe3160201141a1f884ff63048d9d9b
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-07-07 21:33:56 -0400

    Linking multiple button to the function

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.calculator
 
-import  android.os.Bundle
+import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var outputTextView: TextView
     private lateinit var conversionSpinner: Spinner
     private lateinit var convertButton: Button
-// added array for spinner to selected by user
+
     private val conversionOptions = arrayOf(
         "cm to in",
         "in to cm",
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         "ml to L",
         "L to ml"
     )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,7 +39,6 @@ class MainActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, conversionOptions)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         conversionSpinner.adapter = adapter
-
 
         conversionSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
@@ -57,46 +57,41 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-
-
-
     private fun convert() {
-            val inputText = inputEditText.text.toString().trim()
-            if (inputText.isNotEmpty()) {
-                val inputValue = inputText.toDouble()
+        val inputText = inputEditText.text.toString().trim()
+        if (inputText.isNotEmpty()) {
+            val inputValue = inputText.toDouble()
 
-                val conversionType = conversionSpinner.selectedItem.toString()
-                val result: Double
+            val conversionType = conversionSpinner.selectedItem.toString()
+            val result: Double
 
-                when (conversionType) {
-                    "cm to in" -> {
-                        result = inputValue / 2.54
-                        outputTextView.text = result.toString()
-                    }
-                    "in to cm" -> {
-                        result = inputValue * 2.54
-                        outputTextView.text = result.toString()
-                    }
-                    "km to m" -> {
-                        result = inputValue * 1000
-                        outputTextView.text = result.toString()
-                    }
-                    "m to km" -> {
-                        result = inputValue / 1000
-                        outputTextView.text = result.toString()
-                    }
-                    "ml to L" -> {
-                        result = inputValue / 1000
-                        outputTextView.text = result.toString()
-                    }
-                    "L to ml" -> {
-                        result = inputValue * 1000
-                        outputTextView.text = result.toString()
-                    }
-
+            when (conversionType) {
+                "cm to in" -> {
+                    result = inputValue / 2.54
+                    outputTextView.text = result.toString()
+                }
+                "in to cm" -> {
+                    result = inputValue * 2.54
+                    outputTextView.text = result.toString()
+                }
+                "km to m" -> {
+                    result = inputValue * 1000
+                    outputTextView.text = result.toString()
+                }
+                "m to km" -> {
+                    result = inputValue / 1000
+                    outputTextView.text = result.toString()
+                }
+                "ml to L" -> {
+                    result = inputValue / 1000
+                    outputTextView.text = result.toString()
+                }
+                "L to ml" -> {
+                    result = inputValue * 1000
+                    outputTextView.text = result.toString()
                 }
             }
+        }
     }
-
 }
+
